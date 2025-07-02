f0b4a38e59cdd65bc5debae8c35100dd47178fee
Théo Renard <88674589+Dastfox@users.noreply.github.com>
2025-07-02 17:07:23 +0200

    Add modular Angular/FastAPI character builder

---
+++ frontend/src/app/app.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: '<h1>Mappa Mundi Character Builder</h1><character-form></character-form>'
+})
+export class AppComponent {}
