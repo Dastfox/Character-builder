9bddc626514ed4c46ce9dba52c6c08796ca09056
trenard <trenard@onestock-retail.com>
2025-07-03 07:19:24 +0200

    init

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,16 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { CharacterFormComponent } from './character-form.component';
 import { ApiService } from './api.service';
 
-@NgModule({
-  declarations: [AppComponent, CharacterFormComponent],
-  imports: [BrowserModule, FormsModule, HttpClientModule],
-  providers: [ApiService],
-  bootstrap: [AppComponent]
-})
+@NgModule({ declarations: [AppComponent, CharacterFormComponent],
+    bootstrap: [AppComponent], imports: [BrowserModule, FormsModule], providers: [ApiService, provideHttpClient(withInterceptorsFromDi())] })
 export class AppModule {}
